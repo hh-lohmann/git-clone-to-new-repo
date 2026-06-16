9e07437306110c870ea97fdca00b5479290ef1f1
hh.skladby <hh.skladby@gmail.com>
2026-06-16 17:13:03 +0200

    Housekeeping

--- testo.js
+++
@@ -1,3 +0,0 @@
-import { gitCloneToNewRepo } from './index.js'
-
-gitCloneToNewRepo( 'https://github.com/hh-lohmann/template-gh', '/home/hh-skladby/00_hh/hh-skladby/concepts/readme-references', 'dev' )
